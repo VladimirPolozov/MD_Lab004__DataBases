0c5193574cfed1fe18b5851d6de3263bab90d4bc
Vladimir Polozov <z2005q.polozov@yandex.ru>
2025-05-31 03:12:55 +0700

    Added dependency injection and view model

---
+++ app/src/main/java/com/example/md_lab004__databases/NoteApp.kt
@@ -0,0 +1,7 @@
+package com.example.md_lab004__databases
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteApp : Application()
---
+++ app/src/main/java/com/example/md_lab004__databases/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.example.md_lab004__databases.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.md_lab004__databases.data.repository.NoteRepositoryImpl
+import com.example.md_lab004__databases.data.source.NoteDatabase
+import com.example.md_lab004__databases.domain.repository.NoteRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+}
